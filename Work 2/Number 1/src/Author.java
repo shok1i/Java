816cc085b5e436fab886deb7ea43dbd280ddfa92
shokii <shilo152004@mail.ru>
2023-09-01 23:55:36 +0300

    Number 2 start

---
+++ Work 2/Number 1/src/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+    private String name, inbox;
+    private char gender;
+
+    public Author(String name, String inbox, char gender){
+        this.name = name;
+        this.inbox = inbox;
+        this.gender = gender;
+    }
+
+}
