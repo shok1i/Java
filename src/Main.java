1b51855e0df41bf7e608c68aecad3261a38cf616
shokii <shilo152004@mail.ru>
2023-09-01 21:56:52 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args) {  }
+    public static void main(String[] args) { num_3(); }
 
     // Задание 3
     private static void num_3 () {
