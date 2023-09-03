3e09ddd8a6ed26d7c99ae0ba92f3f44e26fd452a
shokii <shilo152004@mail.ru>
2023-09-03 18:26:11 +0300

    Начало 2.5

---
+++ src/Work_2/Number_5/DogKennel.java
@@ -0,0 +1,11 @@
+package Work_2.Number_5;
+
+import java.util.ArrayList;
+
+public class DogKennel {
+
+    public static void main(String[] args) {
+        // Список для питомника
+        ArrayList<Dog> Kennel = new ArrayList<>();
+    }
+}
